e1a6fcf21c7bfb965f708a7e02b84237b28adc65
martina <martinaromeroscro17@gmail.com>
2026-05-03 22:07:17 -0300

    Quinto problema

--- Dsw2026Ej8/Problema4.cs
+++ Dsw2026Ej8/Problema4.cs
@@ -6,5 +6,31 @@ namespace Dsw2026Ej8
 {
     internal class Problema4
     {
+        public double CalcularPromedio(int? nota1, int? nota2, int? nota3)
+        {
+            int suma = 0;
+            int contador = 0;
+
+        
+            void ProcesarNota(int? nota)
+            {
+                if (nota.HasValue && nota.Value >= 0 && nota.Value <= 10)
+                {
+                    suma += nota.Value;
+                    contador++;
+                }
+            }
+
+            ProcesarNota(nota1);
+            ProcesarNota(nota2);
+            ProcesarNota(nota3);
+
+            if (contador == 0)
+                return 0;
+
+            return (double)suma / contador;
+        }
+
     }
 }
+
--- Dsw2026Ej8/Product.cs
+++ Dsw2026Ej8/Product.cs
@@ -6,5 +6,16 @@ namespace Dsw2026Ej8
 {
     internal class Product
     {
+        public string Description { get; private set; }
+
+        public Product(string description)
+        {
+            Description = description;
+        }
+
+        public void CambiarDescripcion(string nuevaDescripcion)
+        {
+            Description = nuevaDescripcion;
+        }
     }
 }
